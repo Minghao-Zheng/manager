e6b91365883f918e852b3595e88c5e3fe7bf33bc
Minghao-Zheng <3899808472@qq.com>
2025-02-07 19:38:58 +0800

    change lab1 to a buggy one to practise git

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,13 +3,14 @@
  */
 public class Collatz {
 
-    /** if n is an even number, the next number is n/2.
-     *  else, the next number is 3*n+1. */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
-        } else {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
